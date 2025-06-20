9757df7df82b5f00226e796ddc8815d4e545a216
Web-36 <Web-36>
2025-06-20 08:36:49 +0700

    fix entryPoint

--- Web-36/vite.config.ts
+++ Web-36/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+function getEntriesFromDir(dirPath: string): Record<string, string> {
+  return Object.fromEntries(
+    fs
+      .readdirSync(dirPath)
+      .filter((name) => fs.statSync(path.join(dirPath, name)).isDirectory())
+      .map((name) => [name, path.join(dirPath, name, `${name}.ts`)])
+  );
+}
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: "src/Components/Header",
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: getEntriesFromDir(path.resolve(__dirname, "src/Components")),
+      output: { entryFileNames: "[name]/[name].[format].js" },
+    },
+  },
 });
