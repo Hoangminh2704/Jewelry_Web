09c87b4f0af56780453cd2a132b60a01c043fe68
Web-36 <Web-36>
2025-06-26 14:19:16 +0700

    update Features section

--- Web-36/src/Components/Home/Home.ts
+++ Web-36/src/Components/Home/Home.ts
@@ -1,1 +1,37 @@
-import './Home.scss'
+// import "./Home.scss";
+
+// document.addEventListener(
+//   "dblclick",
+//   function (event) {
+//     event.preventDefault();
+//   },
+//   { passive: false }
+// );
+
+// const hamburgerButtons = document.querySelectorAll(".header-hamberger-mobile");
+// const menu = document.querySelector(".open");
+
+// const handleToggle = (event) => {
+//   event.preventDefault();
+//   const button = event.currentTarget;
+
+//   button.classList.toggle("is-active");
+//   if (menu) {
+//     menu.classList.toggle("oppenned");
+//   }
+// };
+
+// hamburgerButtons.forEach((button) => {
+//   button.addEventListener("click", handleToggle);
+// });
+
+// const menuLinks = document.querySelectorAll(".header--sub-menu li a");
+
+// menuLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     if (menu) {
+//       menu.classList.remove("oppenned");
+//     }
+//     hamburgerButtons.forEach((btn) => btn.classList.remove("is-active"));
+//   });
+// });
