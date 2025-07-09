4e4044ac4981b47052fd0dcfa07637180067cdc1
Web-36 <Web-36>
2025-07-09 17:17:18 +0700

    filter exist ts file

--- Web-36/vite.config.ts
+++ Web-36/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import fs from "fs";
 import path from "path";
+
 function getEntriesFromDir(dirPath: string): Record<string, string> {
   return Object.fromEntries(
     fs
       .readdirSync(dirPath)
       .filter((name) => fs.statSync(path.join(dirPath, name)).isDirectory())
+      .filter((name) => {
+        const tsFilePath = path.join(dirPath, name, `${name}.ts`);
+        return fs.existsSync(tsFilePath);
+      })
       .map((name) => [name, path.join(dirPath, name, `${name}.ts`)])
   );
 }
